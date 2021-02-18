35b2acead232c60ea34ce21a7fb7c66cc2b5d2d8
Maddie Higgins <maddiehiggins9@gmail.com>
2021-02-18 10:32:05 -0600

    randomize choices

--- 13-random-choice-picker/script.js
+++ 13-random-choice-picker/script.js
@@ -5,6 +5,13 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (evt) => {
   createTags(evt.target.value);
+
+  if (evt.key === 'Enter') {
+    setTimeout(() => {
+      evt.target.value = '';
+    }, 10);
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -20,4 +27,40 @@ function createTags(input) {
       tagEl.innerText = tag;
       tagsEl.appendChild(tagEl);
     });
+}
+
+function randomSelect() {
+  // number of times to highlight each choice before stopping
+  const times = 30;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    });
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
